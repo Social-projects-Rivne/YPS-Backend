1e81c9696060e20ace13a3a48b80c3ad512c953e
Redingston <kondratukktc@gmail.com>
2020-01-28 14:15:29 +0200

    create admin controller

---
+++ src/YPS.Application/Admins/Queries/GetAdmin/AdminViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPS.Application.Admins.Queries.GetAdmin
+{
+    public class AdminViewModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ src/YPS.Application/Admins/Queries/GetAdmin/GetAdminQuery.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YPS.Application.Interfaces;
+
+namespace YPS.Application.Admins.Queries.GetAdmin
+{
+    public class GetAdminQuery: IRequest<AdminViewModel>
+    {
+        public long Id;
+        public class GetAdminQueryHandler : IRequestHandler<GetAdminQuery, AdminViewModel>
+        {
+            private readonly IYPSDbContext _dbContext;
+            private readonly IMapper _mapper;
+
+            public GetAdminQueryHandler(IYPSDbContext dbContext, IMapper mapper)
+            {
+                _dbContext = dbContext;
+                _mapper = mapper;
+            }
+
+            public async Task<AdminViewModel> Handle(GetAdminQuery request, CancellationToken cancellationToken)
+            {
+               var admin = await _dbContext.Users.FirstOrDefaultAsync(
+                    x => x.Id == request.Id);
+                return new AdminViewModel
+                {
+                    Id = admin.Id,
+                    FirstName = admin.FirstName,
+                    Surname = admin.Surname,
+                    MiddleName = admin.MiddleName,
+                    Email = admin.Email,
+                    PhoneNumber = admin.PhoneNumber,
+                    DateOfBirth = admin.DateOfBirth
+
+                };
+            }
+        }
+    }
+}
