bfd580d2637eee0cb3a144f6312dd832e9d301b5
Bohdan <hrabarchuk.bohdan2000@gmail.com>
2020-01-31 15:04:00 +0200

    update

---
+++ src/YPS.Application/School/Request/Command/Register/RegisterSchoolCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace YPS.Application.SchoolRequest.Command.Register
+{
+    public sealed class RegisterSchoolCommand : IRequest<long>
+    {
+        
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Locality { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumb { get; set; }
+        
+        public bool Confirmation { get; set; }
+    }
+}
---
+++ src/YPS.WebUI/Controllers/SchoolRequestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YPS.Application.SchoolRequest.Command.Register;
+
+namespace YPS.WebUI.Controllers
+{
+
+    public class SchoolRequestController : ApiController
+    {
+        
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<long>> Create([FromBody] RegisterSchoolCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var account = await Mediator.Send(command).ConfigureAwait(false);
+                return Ok(account);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+
+
+        }
+    }
+}
