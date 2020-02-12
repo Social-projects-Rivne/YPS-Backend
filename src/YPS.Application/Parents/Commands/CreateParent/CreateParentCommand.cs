a52432b668ccf03722c76d912bba531a0a7d3192
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-02-12 22:44:43 +0200

    fuck

--- src/YPS.Application/Parents/Commands/CreateParent/CreateParentCommand.cs
+++ src/YPS.Application/Parents/Commands/CreateParent/CreateParentCommand.cs
@@ -4,15 +4,45 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using YPS.Application.Interfaces;
+using YPS.Application.Models;
+using YPS.Domain.Entities;
 
 namespace YPS.Application.Parents.Commands.CreateParent
 {
     public sealed class CreateParentCommand : IRequest<long>
     {
+        public UserPartial User { get; set; }
+        public string WorkInfo { get; set; }
+        public long SchoolId { get; set; }
+
         public sealed class CreateParentCommandHandler : IRequestHandler<CreateParentCommand, long>
         {
-            public Task<long> Handle(CreateParentCommand request, CancellationToken cancellationToken)
+            private readonly IYPSDbContext _context;
+            private readonly IUserService _userService;
+
+            public CreateParentCommandHandler(IYPSDbContext context, IUserService userService)
             {
+                _context = context;
+                _userService = userService;
+            }
+            public async Task<long> Handle(CreateParentCommand request, CancellationToken cancellationToken)
+            {
+                User createdUser = await _userService.CreateUser(request.User);
+
+                if (createdUser != null)
+                {
+                    Parent parent = new Parent
+                    {
+                        UserId = createdUser.Id,
+                        SchoolId = 1
+                    };
+
+                    _context.Parents.Add(parent);
+                    await _context.SaveChangesAsync(cancellationToken);
+                }
+
+                return createdUser.Id;
             }
         }
     }
--- src/YPS.Application/Teachers/Commands/CreateTeacher/CreateTeacherCommand.cs
+++ src/YPS.Application/Teachers/Commands/CreateTeacher/CreateTeacherCommand.cs
@@ -4,15 +4,48 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using YPS.Application.Interfaces;
+using YPS.Application.Models;
+using YPS.Domain.Entities;
 
 namespace YPS.Application.Teachers.Commands.CreateTeacher
 {
     public sealed class CreateTeacherCommand : IRequest<long>
     {
+        public UserPartial User { get; set; }
+        public string Degree { get; set; }
+        public long RoleId { get; set; }
+        public long SchoolId { get; set; }
+
         public sealed class CreateTeacherCommandHandler : IRequestHandler<CreateTeacherCommand, long>
         {
+            private readonly IYPSDbContext _context;
+            private readonly IUserService _userService;
+
+            public CreateTeacherCommandHandler(IYPSDbContext context, IUserService userService)
+            {
+                _context = context;
+                _userService = userService;
+            }
+
             public async Task<long> Handle(CreateTeacherCommand request, CancellationToken cancellationToken)
             {
+                User createdUser = await _userService.CreateUser(request.User);
+
+                if (createdUser != null)
+                {
+                    Teacher teacher = new Teacher
+                    {
+                        UserId = createdUser.Id,
+                        Degree = request.Degree,
+                        SchoolId = 1
+                    };
+
+                    _context.Teachers.Add(teacher);
+                    await _context.SaveChangesAsync(cancellationToken);
+                }
+
+                return createdUser.Id;
             }
         }
     }
