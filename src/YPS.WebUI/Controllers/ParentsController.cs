7310a74c5315b9d6701487e584ed15b989683513
Black Reaver <losha.pisnyak42@gmail.com>
2020-02-13 22:17:09 +0200

    fixed bugs

--- src/YPS.WebUI/Controllers/ParentsController.cs
+++ src/YPS.WebUI/Controllers/ParentsController.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using YPS.Application.Parents.Commands.CreateParent;
+using YPS.Application.Parents.Queries;
+using YPS.Application.Parents.ViewModels;
 
 namespace YPS.WebUI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
     public class ParentsController : ApiController
     {
+        [HttpGet]
+        public async Task<ActionResult<ICollection<ParentViewModel>>> GetParents([FromQuery]GetParentsQuery command)
+        {
+            var claim = User.Claims.FirstOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname", StringComparison.InvariantCultureIgnoreCase));
+            if (claim != null)
+            {
+                var viewModel = await Mediator.Send(new GetParentsQuery { Id = long.Parse(claim.Value) });
+                return Ok(viewModel);
+            }
+            return BadRequest($"Unatorized");
+        }
+
         [HttpPost]
         public async Task<ActionResult<long>> Create([FromBody] CreateParentCommand command)
         {
