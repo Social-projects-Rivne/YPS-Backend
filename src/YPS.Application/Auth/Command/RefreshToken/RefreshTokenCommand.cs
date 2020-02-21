cc0f76d4e7a682bd94dea6e226a47b7ac507e35b
Bohdan <hrabarchuk.bohdan2000@gmail.com>
2020-02-21 13:10:50 +0200

    update1

--- src/YPS.Application/Auth/Command/RefreshToken/RefreshTokenCommand.cs
+++ src/YPS.Application/Auth/Command/RefreshToken/RefreshTokenCommand.cs
@@ -7,8 +7,16 @@ namespace YPS.Application.Auth.Command.RefreshToken
 {
     public sealed class RefreshTokenCommand : IRequest<RefreshTokenViewModel>
     {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
+        public string ApiKey { get; }
+        public string Token { get; }
+        public string RefreshToken { get; }
 
+        public RefreshTokenCommand(string apiKey, string token, string refreshToken)
+        {
+            ApiKey = apiKey;
+            Token = token;
+            RefreshToken = refreshToken;
+        }
     }
+
 }
