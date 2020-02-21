1e8290c89b6b8e4f3ef97927a62697f35b64bbc3
illya.shagoferov <shagoferov@gmail.com>
2020-02-21 17:36:51 +0200

    add more functionality to query

--- src/YPS.Application/Parents/Queries/GetParentProfileInfo/GetParentProfileInfoVm.cs
+++ src/YPS.Application/Parents/Queries/GetParentProfileInfo/GetParentProfileInfoVm.cs
@@ -14,14 +14,26 @@ namespace YPS.Application.Parents.ViewModels
         public long UserId { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string ParentFullName { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public string SchoolName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string ImageUrl { get; set; }
+        public string Workinfo { get; set; }
    
     public void Mapping(Profile profile)
     {
-        profile.CreateMap<Parent, GetParentProfileInfoVm>() 
-            .ForMember(x => x.Email, opt => opt.MapFrom(x => x.User.Email))
-            .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(x => x.User.PhoneNumber))
-            .ForMember(x => x.ParentFullName, opt => opt.MapFrom(x => $"{x.User.FirstName} {x.User.Surname} {x.User.MiddleName}"));
+            profile.CreateMap<Parent, GetParentProfileInfoVm>()
+                .ForMember(x => x.Email, opt => opt.MapFrom(x => x.User.Email))
+                .ForMember(x => x.PhoneNumber, opt => opt.MapFrom(x => x.User.PhoneNumber))
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(x => x.User.FirstName))
+                .ForMember(x => x.Surname, opt => opt.MapFrom(x => x.User.Surname))
+                .ForMember(x => x.MiddleName, opt => opt.MapFrom(x => x.User.MiddleName))
+                .ForMember(x => x.SchoolName, opt => opt.MapFrom(x => x.User.School.ShortName))
+                .ForMember(x => x.DateOfBirth, opt => opt.MapFrom(x => x.User.DateOfBirth.ToString("dd-MM-yyyy")))
+                .ForMember(x => x.ImageUrl, opt => opt.MapFrom(x => x.User.ImageUrl))
+                .ForMember(x => x.Workinfo, opt => opt.MapFrom(x => x.WorkInfo));
         }
     }
 }
