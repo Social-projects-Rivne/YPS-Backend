0bb313774ac6bf0af37c07457112c66b274bb3d9
Karonuk <losha.pisnyak42@gmail.com>
2020-02-26 12:23:00 +0200

    design changes

--- src/YPS.Application/Headmasters/Commands/CheckMasterRegisterLink/CheckMasterRegisterLinkCommand.cs
+++ src/YPS.Application/Headmasters/Commands/CheckMasterRegisterLink/CheckMasterRegisterLinkCommand.cs
@@ -36,8 +36,7 @@ namespace YPS.Application.HeadMasters.Command.CheckMasterRegisterLink
                 else
                 {
                     return false;
-                }
-                
+                }                
             }
         }
     }
