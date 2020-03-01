b866c9828c29cb03088ee88606b60245dd43458c
Vitaliy <vetalkasmile@gmail.com>
2020-03-01 16:03:38 +0300

    qw

---
+++ src/YPS.Application/Headmasters/Queries/CheckHeadMasterRegisterLink/CheckHeadMasterRegisterLinkQuery.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YPS.Application.Interfaces;
+
+namespace YPS.Application.HeadMasters.Queries.CheckMasterRegisterLink
+{
+    public class CheckHeadMasterRegisterLinkQuery : IRequest<bool>
+    {
+        public string Link { get; set; }
+
+        public class CheckHeadMasterRegisterLinkQueryHandler : IRequestHandler<CheckHeadMasterRegisterLinkQuery, bool>
+        {
+            private readonly IYPSDbContext _dbContext;
+
+            public CheckHeadMasterRegisterLinkQueryHandler(IYPSDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public async Task<bool> Handle(CheckHeadMasterRegisterLinkQuery request, CancellationToken cancellationToken)
+            {
+                var school = await _dbContext.Schools.FirstOrDefaultAsync(x => x.RegistrationLink == request.Link);
+                return school != null ? true : false;
+            }
+        }
+    }
+}
