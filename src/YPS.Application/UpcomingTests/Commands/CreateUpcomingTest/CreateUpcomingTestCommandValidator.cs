dc85ab7d6507501259c1ffab3a095e086accd631
Vitaliy <vetalkasmile@gmail.com>
2020-03-01 17:10:16 +0300

    AddedValidation

--- src/YPS.Application/UpcomingTests/Commands/CreateUpcomingTest/CreateUpcomingTestCommandValidator.cs
+++ src/YPS.Application/UpcomingTests/Commands/CreateUpcomingTest/CreateUpcomingTestCommandValidator.cs
@@ -7,6 +7,18 @@ namespace YPS.Application.UpcomingTests.Commands.CreateUpcomingTest
 {
     public class CreateUpcomingTestCommandValidator : AbstractValidator<CreateUpcomingTestCommand>
     {
+        public CreateUpcomingTestCommandValidator()
+        {
+            RuleFor(v => v.ScheduledDate >= DateTime.Now.AddDays(1))
+                .NotEmpty();
 
+            RuleFor(v => v.Topic)
+                .MaximumLength(254)
+                .NotEmpty();
+
+            RuleFor(v => v.TestType)
+                .MaximumLength(254)
+                .NotEmpty();
+        }
     }
 }
