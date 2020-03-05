ea41767722151435124b557baea55e18aaf352bd
Karonuk <losha.pisnyak42@gmail.com>
2020-03-05 14:27:13 +0200

    added get query

---
+++ src/YPS.Application/Teachers/Queries/GetTeachersByDiscipline/TeachersByDisciplineVm.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YPS.Application.Mapping;
+using YPS.Domain.Entities;
+
+namespace YPS.Application.Teachers.Queries.GetTeachersByDiscipline
+{
+    public class TeachersByDisciplineVm : IMapFrom<Teacher>
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Teacher, TeachersByDisciplineVm>()
+                .ForMember(x => x.Id, opt => opt.MapFrom(x => x.Id))
+                .ForMember(x => x.FullName, opt => opt.MapFrom(x => x.User.FirstName + " " + x.User.Surname + " " + x.User.MiddleName));
+        }
+    }
+}
