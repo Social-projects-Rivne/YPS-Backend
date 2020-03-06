4a893501ab75e69f5af8b7b59b678ee7a4640dd7
illya.shagoferov <shagoferov@gmail.com>
2020-03-06 13:57:33 +0200

    added spaces

--- src/YPS.Domain/Entities/Auditorium.cs
+++ src/YPS.Domain/Entities/Auditorium.cs
@@ -9,8 +9,10 @@ namespace YPS.Domain.Entities
     {
         public long Number { get; set; }
         public string Name { get; set; }
+        
         public long SchoolId { get; set; }
         public School School { get; set; }
+        
         public ICollection<Lesson> Lessons { get; set; }
     }
 }
