d2c80f5bd3cfe1e77b76f26406791de7015c5e03
Black Reaver <losha.pisnyak42@gmail.com>
2020-03-08 15:16:32 +0200

    added all methods to SchoolRequestController  from SchoolRequestProcesingController

--- src/YPS.WebUI/Controllers/SchoolRequestsController.cs
+++ src/YPS.WebUI/Controllers/SchoolRequestsController.cs
@@ -5,12 +5,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using YPS.Application.SchoolRequests.Commands.ApproveSchoolRequest;
 using YPS.Application.SchoolRequests.Commands.CreateSchoolRequest;
+using YPS.Application.SchoolRequests.Commands.DisapproveSchoolRequest;
+using YPS.Application.SchoolRequests.Queries.GetUnviewedSchoolRequests;
 
 namespace YPS.WebUI.Controllers
 {
     public class SchoolRequestsController : ApiController
     {
+        [HttpPost("[action]")]
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<SchoolViewModel>> Approve([FromBody] ApproveSchoolRequestCommand command)
+        {
+            var response = await this.Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<RemoveRequestViewModel>> Disapprove([FromQuery] DisapproveSchoolRequestCommand command)
+        {
+            var response = await this.Mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<ActionResult<ICollection<SchoolRequestVm>>> GetSchoolRequests()
+        {
+            var vm = await this.Mediator.Send(new GetUnviewedSchoolRequestsQuery());
+            return Ok(vm);
+        }
+
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
