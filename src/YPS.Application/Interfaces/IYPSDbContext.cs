0cacf4013b6b521af688af8fd0405e7b5c4337af
illya.shagoferov <shagoferov@gmail.com>
2020-03-09 14:58:44 +0200

    added get auditoriums query

--- src/YPS.Application/Interfaces/IYPSDbContext.cs
+++ src/YPS.Application/Interfaces/IYPSDbContext.cs
@@ -32,6 +32,7 @@ namespace YPS.Application.Interfaces
         DbSet<Role> Roles { get; set; }
         DbSet<SchoolRequest> SchoolRequests { get; set; }
         DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+        DbSet<Auditorium> Auditoriums { get; set; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
