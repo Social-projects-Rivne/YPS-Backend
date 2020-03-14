987f6e7f55851d1e8a1110330c2ef76cfd5755f7
Redingston <kondratukktc@gmail.com>
2020-03-14 12:27:34 +0200

    creat command

---
+++ src/YPS.Application/Lessons/Commands/CreateJournalColumn/CreateJournalColumnCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPS.Application.Lessons.Commands.CreateJournalColumn
+{
+    public class CreateJournalColumnCommand
+    {
+    }
+}
