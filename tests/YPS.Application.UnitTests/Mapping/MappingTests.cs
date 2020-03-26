af335465119b321c1548d8e53f5486e130b0a37a
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-26 21:26:49 +0200

    set up testing

---
+++ tests/YPS.Application.UnitTests/Mapping/MappingTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System;
+using Xunit;
+using YPS.Application.Parents.Queries.GetParentsBySchool;
+using YPS.Application.Pupils.Queries.GetPupilsBySchool;
+using YPS.Domain.Entities;
+
+namespace YPS.Application.UnitTests.Mapping
+{
+    public class MappingTests : IClassFixture<MappingTestsFixture>
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests(MappingTestsFixture fixture)
+        {
+            _configuration = fixture.ConfigurationProvider;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+        
+        [Theory]
+        [InlineData(typeof(User), typeof(PupilBySchoolVm))]
+        [InlineData(typeof(User), typeof(ParentBySchoolVm))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            object instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+    }
+}
---
+++ tests/YPS.Application.UnitTests/YPSDbContextFactory.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+using YPS.Persistence;
+
+namespace YPS.Application.UnitTests
+{
+    public static class YPSDbContextFactory
+    {
+        public static YPSDbContext Create()
+        {
+            DbContextOptions<YPSDbContext> options = new DbContextOptionsBuilder<YPSDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            YPSDbContext context = new YPSDbContext(options);
+
+            context.Database.EnsureCreated();
+
+
+            return context;
+        }
+
+        public static void Destroy(YPSDbContext context)
+        {
+            context.Database.EnsureDeleted();
+
+            context.Dispose();
+        }
+    }
+}
