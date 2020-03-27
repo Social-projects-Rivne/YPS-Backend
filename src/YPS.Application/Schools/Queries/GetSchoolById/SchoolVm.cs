1cea5b19c4529bc0540ef74da5b1db4b48021efe
Dmitriy Movchaniuk <dmmmov.live@gmail.com>
2020-03-27 17:22:57 +0200

    create GetSchoolByIdQuery

---
+++ src/YPS.Application/Schools/Queries/GetSchoolById/SchoolVm.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YPS.Application.Mapping;
+using YPS.Domain.Entities;
+
+namespace YPS.Application.Schools.Queries.GetSchoolById
+{
+    public sealed class SchoolVm : IMapFrom<School>
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Locality { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string RegistrationLink { get; set; }
+
+        public uint Pupils { get; set; }
+        public uint Teachers { get; set; }
+        public uint Parents { get; set; }
+        public uint Auditoriums { get; set; }
+        public uint Disciplines { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<School, SchoolVm>()
+                .ForMember(x => x.Pupils, opt => opt.MapFrom(s => s.Users.Where(x => x.Role.Name == "pupil").Count()))
+                .ForMember(x => x.Teachers, opt => opt.MapFrom(s => s.Users.Where(x => x.Role.Name == "teacher" || x.Role.Name == "head-asistant").Count()))
+                .ForMember(x => x.Parents, opt => opt.MapFrom(s => s.Users.Where(x => x.Role.Name == "parent").Count()))
+                .ForMember(x => x.Auditoriums, opt => opt.MapFrom(s => s.Auditoriums.Count))
+                .ForMember(x => x.Disciplines, opt => opt.MapFrom(s => s.Disciplines.Count));
+        }
+    }
+}
---
+++ src/YPS.WebUI/Controllers/SchoolsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YPS.Application.Schools.Queries.GetSchoolById;
+
+namespace YPS.WebUI.Controllers
+{
+    public class SchoolsController : ApiController
+    {
+        [Authorize]
+        public async Task<ActionResult<SchoolVm>> GetById()
+        {
+            long schoolId = long.Parse(User.FindFirstValue(ClaimTypes.GivenName));
+            return Ok(await Mediator.Send(new GetSchoolByIdQuery { SchoolId = schoolId }));
+        }
+    }
+}
