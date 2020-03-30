ff2e31a11ad358c7359e3fa82d61dc346905e726
Redingston <kondratukktc@gmail.com>
2020-03-30 10:45:31 +0300

    init

---
+++ tests/YPS.Application.UnitTests/Disciplines/Queries/GetDisciplinesByTeacher/GetDisciplinesByTeacherTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YPS.Application.Disciplines.Queries.GetDisciplinesByTeacher;
+using YPS.Persistence;
+using static YPS.Application.Disciplines.Queries.GetDisciplinesByTeacher.GetDisciplinesByTeacherQuery;
+using Xunit;
+using Shouldly;
+using YPS.Domain.Entities;
+using System.Linq;
+
+namespace YPS.Application.UnitTests.Disciplines.Queries.GetDisciplinesByTeacher
+{
+    public class GetDisciplinesByTeacherTests
+    {
+        public YPSDbContext _context { get; }
+        public IMapper _mapper { get; }
+
+        public GetDisciplinesByTeacherTests(QueryTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _mapper = fixture.Mapper;
+        }
+
+       [Fact]
+        public async Task Handle_ReturnsCocrectVmAndListCount() {
+
+            long testTeacherId = 5;
+            var user = _context.Teachers.SingleOrDefault(x => x.Id == testTeacherId);
+            var query = new GetDisciplinesByTeacherQuery();
+            var handle = new GetDisciplinesByTeacherQueryHandler(_context, _mapper);
+            var result = await handle.Handle(query, CancellationToken.None);
+
+            result.ShouldBeOfType<DisciplineVm>();
+        } 
+    }
+}
