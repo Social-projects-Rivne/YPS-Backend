61221620682ea25f4a955355105a2755595a115c
Redingston <kondratukktc@gmail.com>
2020-03-30 12:35:13 +0300

    change theory to fact

--- tests/YPS.Application.UnitTests/Disciplines/Queries/GetDisciplinesByTeacher/GetDisciplinesByTeacherTests.cs
+++ tests/YPS.Application.UnitTests/Disciplines/Queries/GetDisciplinesByTeacher/GetDisciplinesByTeacherTests.cs
@@ -6,14 +6,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using YPS.Application.Disciplines.Queries.GetDisciplinesByTeacher;
 using YPS.Persistence;
-using static YPS.Application.Disciplines.Queries.GetDisciplinesByTeacher.GetDisciplinesByTeacherQuery;
+using YPS.Application.UnitTests.Mapping;
 using Xunit;
 using Shouldly;
 using YPS.Domain.Entities;
 using System.Linq;
+using static YPS.Application.Disciplines.Queries.GetDisciplinesByTeacher.GetDisciplinesByTeacherQuery;
 
 namespace YPS.Application.UnitTests.Disciplines.Queries.GetDisciplinesByTeacher
 {
+    [Collection("QueryTests")]
     public class GetDisciplinesByTeacherTests
     {
         public readonly YPSDbContext _context;
@@ -25,16 +27,18 @@ namespace YPS.Application.UnitTests.Disciplines.Queries.GetDisciplinesByTeacher
             _mapper = fixture.Mapper;
         }
 
-       [Fact]
-        public async Task Handle_ReturnsCocrectVmAndListCount() {
+        [Fact]
+        public async Task Handle_ReturnsCocrectVmAndListCount() 
+        {
+            var query = new GetDisciplinesByTeacherQuery { TeacherId = 5 };
 
-            long testTeacherId = 5;
-            var user = _context.Teachers.SingleOrDefault(x => x.Id == testTeacherId);
-            var query = new GetDisciplinesByTeacherQuery();
             var handle = new GetDisciplinesByTeacherQueryHandler(_context, _mapper);
+
             var result = await handle.Handle(query, CancellationToken.None);
 
-            result.ShouldBeOfType<DisciplineVm>();
+            result.ShouldBeOfType<List<DisciplineVm>>();
+
+            result.Count.ShouldBe(1);
         } 
     }
 }
